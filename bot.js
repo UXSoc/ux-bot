08843f9af334fe7810c62c3dacdf5798633ee349
hat <jburns@chapman.edu>
2018-09-09 14:21:07 -0700

    adding url message

--- bot.js
+++ bot.js
@@ -146,9 +146,9 @@ if (!process.env.clientId || !process.env.clientSecret) {
         // bot.replyPublic(message,'Everyone can see this part of the slash command');
 
 
-          bot.replyPrivate(message,'Here is your free books: https://www.google.com/, only you can see and access them \n' +
+        bot.replyPrivate(message, 'Here are your free books: https://www.google.com/, only you can see and access them \n' +
               ' Fill in you\'re email and claim');
-
+      });
       controller.on('direct_message,direct_mention,mention', function(bot, message) {
           controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
               if (!convo) {

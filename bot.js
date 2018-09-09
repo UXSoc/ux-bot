da26f18da3b389b7e797df03ef695b9e3d3e1e7f
Xavi Ablaza <xlablaza@gmail.com>
2018-09-09 12:48:54 -0700

    remove public message

--- bot.js
+++ bot.js
@@ -143,7 +143,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
   if (process.env.studio_token) {
       controller.on('slash_command',function(bot,message) {
         // reply to slash command
-        bot.replyPublic(message,'Everyone can see this part of the slash command');
+        // bot.replyPublic(message,'Everyone can see this part of the slash command');
         bot.replyPrivate(message,'Only the person who used the slash command can see this.');
       })
       controller.on('direct_message,direct_mention,mention', function(bot, message) {
